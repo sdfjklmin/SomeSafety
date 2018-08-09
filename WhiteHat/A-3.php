cdbfc777df72d020aab3bdfbe23884bb2d570ed5
sokmin <735305610@qq.com>
2018-08-09 13:56:37 +0800

    Signed-off-by: sokmin <735305610@qq.com>

--- WhiteHat/A-3.php
+++ WhiteHat/A-3.php
@@ -1,3 +1,7 @@
 <?php
+
 $input = $_GET['data'] ?? '<script>alert(32)</script>';
+// var_dump($input) ;exit();
 echo "<div>".$input."</div>";
+// eval($input) ; url: http://base.com/Self/SomeSafety/WhiteHat/A-3.php?data=file_put_contents('test', 'what') ;
+// 利用eval执行对应的PHP脚本 data 参数内容遵循 php 语法
